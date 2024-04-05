f7ada28be1370f77c14b471b2f6a43e4f5a6d75e
IchalAlwiros <ichalwiradev@gmail.com>
2024-04-05 18:07:11 +0700

    initial code

---
+++ cmd/api/routes.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"go_restapi/cmd/api/controllers"
+
+	"github.com/gin-gonic/gin"
+)
+
+func (app *application) routes() *gin.Engine {
+	router := gin.Default()
+
+	// Define the available routes
+	// router.GET("/v1/healthcheck", app.healthcheckHandler)
+	router.GET("/", func(ctx *gin.Context) {
+		ctx.JSON(200, gin.H{
+			"message": "success",
+			"data" : "Welcome TODO Go",
+		})
+	})
+
+
+	router.GET("/todo", controllers.GetAllTodo)
+	router.GET("/todo/:id", controllers.GetOneTodo)
+	router.POST("/todo", controllers.CreateTodo)
+	router.PUT("/todo/:id", controllers.UpdateTodo)
+	router.PATCH("/todo/:id", controllers.PatchTodo)
+	router.DELETE("/todo/:id", controllers.RomoveTodo)
+
+	return router
+}
